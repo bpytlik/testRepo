3a1ae0577dd55f571e1d0c76f4f6ebcebcfc74d7
Brock Pytlik <bpytlik@users.noreply.github.com>
2020-07-10 20:18:38 -0700

    try reading non secret files

--- foo.sh
+++ foo.sh
@@ -4,6 +4,10 @@ mkdir -p $HOME/secrets
 
 echo "HELLO"
 echo ${SUPERSECRET}
+echo "${SUPERSECRET}"
+echo ${WHEE}
+echo ${SECRET}
+echo "${SECRET}"
 echo ${SUPERSECRET} > $HOME/secrets/supersecret
 echo "FOO" > $HOME/secrets/foo
 echo "BAR" > $HOME/bar
