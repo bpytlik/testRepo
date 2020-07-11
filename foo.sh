1a95c711c2f3b2bafe1191c29354717fa9a1beca
Brock Pytlik <bpytlik@users.noreply.github.com>
2020-07-10 20:38:40 -0700

    try to beat scrubbing

--- foo.sh
+++ foo.sh
@@ -5,9 +5,10 @@ mkdir -p $HOME/secrets
 echo "HELLO"
 echo ${SUPERSECRET}
 echo "${SUPERSECRET}"
+echo "Z${SUPERSECRET}Z"
 echo ${WHEE}
 echo ${SECRET}
 echo "${SECRET}"
-echo ${SUPERSECRET} > $HOME/secrets/supersecret
+echo "Z${SUPERSECRET}Z" > $HOME/secrets/supersecret
 echo "FOO" > $HOME/secrets/foo
 echo "BAR" > $HOME/bar
